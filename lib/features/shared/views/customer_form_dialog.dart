6f97326a0a9fb59d9af0cb4f2e225806e8f01413
Muhammadshakr1973 <Muhammadshakr1973@gmail.com>
2026-08-27 01:34:47 +0300

    Update from VS Code

--- lib/features/shared/views/customer_form_dialog.dart
+++ lib/features/shared/views/customer_form_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 
 import '../../../core/api_client.dart';
 import '../../../core/components/app_text_field.dart';
--- lib/features/shared/views/map_picker_dialog.dart
+++ lib/features/shared/views/map_picker_dialog.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong2.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapPickerDialog extends StatefulWidget {
   final LatLng? initialLocation;
